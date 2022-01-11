8f45ce0ad80e83ce5048bc03453a0ee604ecb3d3
DavidMCKim <kimminchang96@gmail.com>
2022-01-11 23:48:40 +0900

    220111 TIL(백준,프로그래머스)

--- 220111 TIL/baekjoon/9243_파일 완전 삭제.py
+++ 220111 TIL/baekjoon/9243_파일 완전 삭제.py
@@ -14,13 +14,3 @@ if normal == result:
     print('Deletion succeeded')
 else:
     print('Deletion failed')
-#
-# N = int(input())
-# a, b = list(input()), list(input())
-# for _ in range(N):
-#     for i in range(len(a)):
-#         if a[i] == '0':
-#             a[i] = '1'
-#         else:
-#             a[i] = '0'
-# print("Deletion succeeded" if a == b else "Deletion failed")
